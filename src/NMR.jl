1e133e04cc6fb7b3b846eb164776d793381b7796
hessammehr <hessammehr@gmail.com>
2017-09-05 19:20:43 +0000

    WIP added.

--- src/NMR.jl
+++ src/NMR.jl
@@ -1,5 +1,31 @@
 module NMR
+export read_bruker_binary
+using Interpolations
 
-# package code goes here
+# Starting point for NMR work in Julia
 
-end # module
+# Bruker I/O functions
+"""
+    read_bruker_binary(fname)
+Bruker FID, and processed frequency-domain data files (fid, 1r, 1i) contain a flat
+binary representation of their data points, each of which is an Int32."""
+function read_bruker_binary(fname)
+    reinterpret(Int32, open(read, fname))
+end
+
+"""
+    interpolate_spect(data, cs_range)"""
+function interpolate_spect(data, cs_range)
+    lo, hi = cs_range
+    N = length(data)
+    f = interpolate(data, BSpline(Cubic(Natural())), OnGrid())
+    # lo -> N, hi -> 1
+    d -> f[N - (d - lo)/(hi - lo)*(N - 1)]
+end
+
+function acq_params(acqufile)
+    matches = eachmatch(r"##\$(.*)=\s?(.*)", open(readstring, acqufile))
+    
+end
+
+end
