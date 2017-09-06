15820ed362de8eb9fbc6232845e756f3adbde709
hessammehr <hessammehr@gmail.com>
2017-09-06 16:22:04 +0000

    Expanded Bruker import WIP

--- src/bruker.jl
+++ src/bruker.jl
@@ -8,7 +8,24 @@ function read_bruker_binary(fname)
     reinterpret(Int32, open(read, fname))
 end
 
+filters = [ ( Set(["SW", "O1", "SFO1"]),
+              float )
+          ]
+
 function acq_params(acqufile)
-    matches = eachmatch(r"##\$(.*)=\s?(.*)", open(readstring, acqufile))
-    
+    matches = eachmatch(r"##\$(.*)=\s?(.*)"m, open(readstring, acqufile))
+    res = Dict(m.captures[1] => parse_param(m.captures[1], m.captures[2]) for m in matches)
+
+    # a few little tweaks
+    res["O1P"] = res["O1"] / res["SFO1"]
+    res
+end
+
+function parse_param(param, val)
+    for (names, fun) in filters
+        if param in names
+            return fun(val)
+        end
+    end
+    return strip(val)
 end
---
+++ src/utils.jl
@@ -0,0 +1,1 @@
+limits(o1p, sw) = (o1p - sw/2, o1
