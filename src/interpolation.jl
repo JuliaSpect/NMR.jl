7c42c4caec42d802dccbb75cdd3d42c3524b19f4
S. Hessam M. Mehr <hessammehr@gmail.com>
2017-09-19 16:33:14 -0700

    Calculation of pulse power profiles.

--- src/interpolation.jl
+++ src/interpolation.jl
@@ -1,7 +1,6 @@
 # Continuous interpolation of discrete frequency-domain data
 
-"""
-    interpolate_spectrum(data, cs_range)"""
+"""interpolate_spectrum(data, cs_range)"""
 function interpolate_spectrum(data, cs_range, options... = [BSpline(Cubic(Natural())), OnGrid()])
     lo, hi = cs_range
     N = length(data)
--- src/utils.jl
+++ src/utils.jl
@@ -1,6 +1,15 @@
 limits(s :: Spectrum) = limits(s.acqu["O1P"], s.acqu["SW"])
 limits(o1p, sw) = (o1p - sw/2, o1p + sw/2)
 
+"""     toppm(f, bf)
+Hz to ppm conversion of a function
+f: Function that takes frequency. (Hz)
+bf: Base frequency. (Hz)
+δ: Chemical shift. (ppm)"""
+function toppm(f, bf)
+    δ -> f(bf*(1.0+δ*1e-6))
+end
+
 ### Plotting functions
 
 import Plots: plot
@@ -12,4 +21,20 @@ function plot(s :: Spectrum, fid :: Bool = false)
     # (shifts, s.re_ft)
     plot(shifts, s.re_ft, xflip=true)
     end
+end
+
+### Pulse power profile
+
+"""     powerprofile(pulse, dt, sfo1)
+Pulse power profile for arbitrary pulse shape.
+pulse: Amplitude profile of pulse.
+dt: 'Sampling frequency' — unit of time between points in time. (s)
+sfo1: Centre frequency of pulse. (Hz)"""
+function powerprofile(pulse, dt, sfo1)
+    ft = abs2.(fft(pulse))
+    l = length(pulse)
+    df = 1/(l*dt)
+    fn = interpolate(ft[1:div(l,2)], BSpline(Cubic(Natural())), OnGrid())
+    max = fn[1]
+    f -> fn[abs(f-sfo1)/df+1]/max
 end
