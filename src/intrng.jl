0da363d0c8f43a83da49c7c15d9552225631ceb8
S. Hessam M. Mehr <hessammehr@gmail.com>
2017-10-19 17:11:09 -0700

    Basic intrng modification functions

--- src/intrng.jl
+++ src/intrng.jl
@@ -6,3 +6,15 @@ end
 function intrng_data(s::Spectrum)
     [s[r] for r in intrng_indices(s)]
 end
+
+function remove_rng(s::Spectrum, δ::Float64)
+    remove_rng(s[s.default_proc], δ)
+end
+
+function remove_rng(p::ProcessedSpectrum, δ)
+    for (i,(hi,lo)) in enumerate(p.intrng)
+        if lo < δ < hi
+            deleteat!(p.intrng, i)
+        end
+    end
+end
