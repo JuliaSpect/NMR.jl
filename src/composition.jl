0879a7a4073d3d101d41f3b06a432b44673006fb
S. Hessam M. Mehr <hessammehr@gmail.com>
2017-10-30 17:59:57 -0700

    Expanded composition operators.

--- src/composition.jl
+++ src/composition.jl
@@ -1,11 +1,28 @@
 #using Interpolations
 
-import Base: +
+import Base: +, -, *, /
 
+for op in (:*, :/)
+    @eval begin
+        function $op(a::T, s::Spectrum) where T <: Number
+            t = deepcopy(s)
+            broadcast!($op, t.fid, t.fid, a)
+            for (_, p) in t.procs
+                broadcast!($op, p.re_ft, p.re_ft, a)
+                broadcast!($op, p.im_ft, p.im_ft, a)
+            end
+            t
+        end
+    end
+end
 
-function +(s1::Spectrum, s2::Spectrum)
-    shifts = union_shifts([s1, s2])
-    vals = resample(s1, shifts) .+ resample(s2, shifts)
-    # TODO: make a proper processed Spectrum
-    (shifts, vals)
+for op in (:+, :-, :*)
+    @eval begin
+        function $op(args::Vararg{Spectrum, N}) where N
+            shifts = union_shifts(collect(args))
+            vals = broadcast($op, (resample(s, shifts) for s in args)...)
+            # TODO: make a proper processed Spectrum
+            (shifts, vals)
+        end
+    end
 end
