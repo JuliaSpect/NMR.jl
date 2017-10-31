f3d8c7a4b22bc895c88732813c439ee655997159
S. Hessam M. Mehr <hessammehr@gmail.com>
2017-10-30 18:03:36 -0700

    Integration plots with custom or alphabetical labels.

--- src/plotting.jl
+++ src/plotting.jl
@@ -6,7 +6,7 @@ plot(s::Spectrum; integrate = false, kw...) = begin
     shifts = linspace(hi, lo, length(s))
     p = plot(color=colorant"#0a3faa",shifts, s[:])
     if integrate == true
-        integration_plot(s, 1)
+        integration_plot(s)
     end
     plot!(xflip=true, yticks=[], leg=false, grid=false, yforeground_color_axis=false; kw...)
 end
@@ -27,7 +27,7 @@ function integral_curve(a::AbstractArray{T,1}, scale::Float64; shift=0.0) where 
     return x
 end
 
-function integration_plot(s::Spectrum, refindex::Int)
+function integration_plot(s::Spectrum, labels::AbstractArray{T}) where T <: AbstractString
     ints = integrate(s)
     if length(ints)==0
         return
@@ -39,6 +39,12 @@ function integration_plot(s::Spectrum, refindex::Int)
     plot!(shifts, [integral_curve(s[ppmtoindex(s,r)], scale) for r in s[s.default_proc].intrng], color=:red, linewidth=1.5, legend=false)
     annotation_height = -0.2maxpoint
     for (i,r) in enumerate(s[s.default_proc].intrng)
-        annotate!([(mean(r),annotation_height,text(sprintf1("%.2f",ints[i]/ints[refindex]),font(10,90.0,colorant"red")))])
+        annotate!([(mean(r), annotation_height, text(labels[i], font(10,90.0,colorant"red")))])
     end
+end
+
+function integration_plot(s::Spectrum)
+    n = length(s[s.default_proc].intrng)
+    labels = collect(string(c) for c in 'a':('a'+n-1))
+    integration_plot(s, labels)
 end
