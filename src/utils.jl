ba0b6eed547e1ecd96da4ab10d40b9c70e551f77
S. Hessam M. Mehr <hessammehr@gmail.com>
2017-11-01 12:07:08 -0700

    Added Int cast.

--- src/utils.jl
+++ src/utils.jl
@@ -34,7 +34,7 @@ end
 
 function ppmtoindex(s::Spectrum, δ)
     min_δ ,max_δ = limits(s)
-    cld(s["SI"]*(max_δ - δ), (max_δ - min_δ))
+    Int(cld(s["SI"]*(max_δ - δ), (max_δ - min_δ)))
 end
 
 function ppmtoindex(s::Spectrum, rng::Tuple{Float64,Float64})
@@ -46,7 +46,7 @@ function ppmtoindex(s::Spectrum, rng::Tuple{Float64,Float64})
     end
 end
 
-hztoindex(f, sw, sf, si) = cld(f*si, sw*sf)
+hztoindex(f, sw, sf, si) = Int(cld(f*si, sw*sf))
 hztoindex(s::Spectrum, f) = hztoindex(f, s["SW"], s["SF"], s["SI"])
 
 Base.length(s::Spectrum) = length(s[:])
