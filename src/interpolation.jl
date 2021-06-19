a9855394d9803bdb687232bb7a58daf43e8b6a43
Hessam Mehr <hessammehr@gmail.com>
2021-06-19 23:07:19 +0100

    Interpolation: Use increasing axis in Interpolation.jl call.

--- src/interpolation.jl
+++ src/interpolation.jl
@@ -4,9 +4,11 @@ import Interpolations: interpolate
 
 function interpolate(s::Spectrum)
     l,h = limits(s)
-    fn = extrapolate(interpolate(s[:], BSpline(Cubic(Natural(OnGrid())))), Flat())
+    fn = extrapolate(interpolate(s[:], BSpline(Cubic(Natural())), OnGrid()), Flat())
+    # Interpolate.jl only supports increasing ranges, so will have to
+    # use flip `h` and `l` here and invert the axis in the next line.
     scaled = scale(fn, range(l; stop=h, length=length(s)))
-    δ -> scaled(h + (l - δ))
+    δ -> scaled[l + (h - δ)]
 end
 
 function resample(s::Spectrum, shifts::AbstractArray)
